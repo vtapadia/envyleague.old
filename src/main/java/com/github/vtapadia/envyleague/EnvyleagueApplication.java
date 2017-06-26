9b1fa56457deef56048883bb949993b5c3e2f7d3
Varesh Tapadia <varesh.tapadia@gmail.com>
2017-06-26 23:29:56 +0200

    Added basic web setup and h2 inmem version

--- src/main/java/com/github/vtapadia/envyleague/EnvyleagueApplication.java
+++ src/main/java/com/github/vtapadia/envyleague/EnvyleagueApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EnvyleagueApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnvyleagueApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EnvyleagueApplication.class, args);
+    }
 }
