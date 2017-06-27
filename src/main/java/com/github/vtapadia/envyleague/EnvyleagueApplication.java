853b7ff75197c29b7ef6cb1b8816c58208e240bf
Varesh Tapadia <varesh.tapadia@gmail.com>
2017-06-27 22:11:02 +0200

    Enabling swagger

--- src/main/java/com/github/vtapadia/envyleague/EnvyleagueApplication.java
+++ src/main/java/com/github/vtapadia/envyleague/EnvyleagueApplication.java
@@ -2,8 +2,10 @@ package com.github.vtapadia.envyleague;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EnvyleagueApplication {
 
     public static void main(String[] args) {
