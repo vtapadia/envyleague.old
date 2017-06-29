7c585b854039ad40d71dab1c96f9b7f4f8e35fa1
Varesh Tapadia <varesh.tapadia@gmail.com>
2017-06-29 22:28:45 +0200

    Added dummy Exception Handler class for now.

---
+++ src/main/java/com/github/vtapadia/envyleague/rest/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.github.vtapadia.envyleague.rest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * TODO Need to implement this for application specific scenarios.
+ * Exception handler for entities
+ */
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Application in illegal state";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
